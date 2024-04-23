0d28103118ab1098728b8bae8015f768c974d5c9
Carlos <ceguito06@gmail.com>
2024-04-23 17:09:48 -0500

    Clases vacias

---
+++ IS_1_SISBO_BlueJ/Boleta_mercado_secundario.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Boleta_mercado_secundario here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Boleta_mercado_secundario
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Boleta_mercado_secundario
+     */
+    public Boleta_mercado_secundario()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
---
+++ IS_1_SISBO_BlueJ/Servicio_adicional.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Servicio_adicional here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Servicio_adicional
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Servicio_adicional
+     */
+    public Servicio_adicional()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
