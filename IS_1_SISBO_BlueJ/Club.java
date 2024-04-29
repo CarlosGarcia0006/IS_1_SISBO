5426a3d2db71cdeffa76b73e2751b9959ba3663b
Carlos <ceguito06@gmail.com>
2024-04-29 09:49:48 -0500

    Club

--- IS_1_SISBO_BlueJ/Club.java
+++ IS_1_SISBO_BlueJ/Club.java
@@ -1,24 +1,39 @@
 
 /**
- * Write a description of class Club here.
+ * El club es el encargado de crear eventos deportivos y tiene a su disposicion diversa informacion sobre los mismos.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Carlos) 
+ * @version (29/04/2024)
  */
 public class Club
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private String nombre;
+    private String correoElectronico;
+    private String estadioPropio;
 
     /**
-     * Constructor for objects of class Club
+     * Constructor vacio.
      */
     public Club()
     {
         // initialise instance variables
-        x = 0;
+        
+    }
+    
+    /**
+     * Constructor completo.
+     */
+    public Club(String nombre, String correoElectronico, String estadioPropio)
+    {
+        // initialise instance variables
+        this.nombre = nombre;
+        this.correoElectronico = correoElectronico;
+        this.estadioPropio = estadioPropio;
     }
 
+    /*Por hacer aun */
+    
     /**
      * An example of a method - replace this comment with your own
      * 
@@ -28,6 +43,31 @@ public class Club
     public int sampleMethod(int y)
     {
         // put your code here
-        return x + y;
+        return 0;
+    }
+    
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public String getCorreo(){
+        return correoElectronico;
+    }
+    
+    public String getEstadio(){
+        return estadioPropio;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public void setCorreo(String correo){
+        this.correoElectronico = correo;
+    }
+    
+    public void setEstadio(String estadio){
+        this.estadioPropio = estadio;
     }
 }
